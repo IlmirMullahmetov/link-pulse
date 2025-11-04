285462d443c46dda92021ffe8deba39cfe6b8f36
IlmirMullahmetov <tex-hon@mail.ru>
2025-11-04 06:33:30 +0300

    frontend setup

---
+++ link-pulse-front-end/app/config/page-url.config.ts
@@ -0,0 +1,7 @@
+class DASHBOARD {
+    private root = '/i'
+
+    HOME = this.root
+    SITES = `${this.root}/sites`
+}
+export const DASHBOARD_PAGES = new DASHBOARD();
---
+++ link-pulse-front-end/app/types/site.types.ts
@@ -0,0 +1,14 @@
+import { IBase } from "./base.types";
+
+export enum EnumSiteStatus {
+    Online = 'online',
+    Offline = 'offline',
+    Unknown = 'unknown'
+}
+
+export interface ISiteResponse extends IBase {
+    name: string;
+    status: EnumSiteStatus;
+}
+
+export type TypeSiteFormState = Partial<Omit<ISiteResponse, 'id' | 'updatedAt'>>
